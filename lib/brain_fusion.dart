e3b7519f431758ea6140f1aaa99dc8816c643431
Copy Boss <copybossai@gmail.com>
2023-06-22 00:20:35 +0500

    upscale

--- lib/brain_fusion.dart
+++ lib/brain_fusion.dart
@@ -3,3 +3,4 @@ library brain_fusion;
 // Export the two main classes of the library
 export 'package:brain_fusion/src/data/ai.dart';
 export 'package:brain_fusion/src/data/enums.dart';
+export 'package:brain_fusion/src/api/upscale.dart';
